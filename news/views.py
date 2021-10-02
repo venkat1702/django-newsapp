649e9bdcf339d765e7ab97a43f322b0d87ac671b
yashwanth venkat <52599813+venkat1702@users.noreply.github.com>
2021-10-02 22:17:09 +0530

    Update views.py

--- news/views.py
+++ news/views.py
@@ -6,6 +6,6 @@ import json
 def home(request):
     import requests
     import json
-    news_api_request=requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=17b835e66adf43fe8ae360dfb2f64f71")
+    news_api_request=requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=(apikey)")
     api=json.loads(news_api_request.content)
     return render(request,'index.html',{'api':api})
